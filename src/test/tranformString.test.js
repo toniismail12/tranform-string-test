622564a4698575a0466e533a689f34d55fc72d26
toniismailz <tonismail20@gmail.com>
2024-03-26 13:23:05 +0700

    update readme

--- src/test/tranformString.test.js
+++ src/test/tranformString.test.js
@@ -22,7 +22,13 @@ describe('convertToUppercase', () => {
     const result = convertToUppercase(inputText);
     expect(result).toBe(expectedOutput);
   });
-  
 
+  it('maks input 10 character', () => {
+    const inputText = "1234567890";
+    const expectedOutput = "1234567890";
+    const result = convertToUppercase(inputText);
+    expect(result).toBe(expectedOutput);
+  });
+  
   // Add more test cases as needed
 });
